5a334994a450d485a0e9fa2a04ba0307f38bc324
David Joseph <davidjoe781227@gmail.com>
2025-02-06 11:47:44 +0100

    fixed issue with menu

--- src/components/homeList.vue
+++ src/components/homeList.vue
@@ -4,117 +4,118 @@ export default{
 }
 </script>
 <template>
-  <div>
-    <div class="absolute  bg-white text-gray-500 right-[70px] z-50 w-[100%] ">
+  <div class="absolute right-[70px] z-50 w-[100%]">
+    <div class="w-full h-10"></div>
+    <div class="text-gray-500 bg-white ">
       <div class="grid grid-cols-5 gap-10 p-4">
-          <div class="border-r ">
-           <h3 class="mb-2 font-semibold">
+        <div class="border-r ">
+          <h3 class="mb-2 font-semibold">
             Home Group
-           </h3>
-           <ul class="space-y-5">
+          </h3>
+          <ul class="space-y-5">
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Decor
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Decor
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Creative
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Creative
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Perfumes
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Perfumes
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Trending
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Trending
+              </a>
             </li>
-           </ul>
-          </div>
-          <div class="border-r">
-           <h3 class="mb-2 font-semibold">
+          </ul>
+        </div>
+        <div class="border-r">
+          <h3 class="mb-2 font-semibold">
             Home Group
-           </h3>
-           <ul class="space-y-5">
+          </h3>
+          <ul class="space-y-5">
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Essentials
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Essentials
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Accessories
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Accessories
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Lookbook
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Lookbook
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Furniture
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Furniture
+              </a>
             </li>
-           </ul>
-          </div>
-          <div class="border-r">
-           <h3 class="mb-2 font-semibold">
+          </ul>
+        </div>
+        <div class="border-r">
+          <h3 class="mb-2 font-semibold">
             Home Group
-           </h3>
-           <ul class="space-y-5">
+          </h3>
+          <ul class="space-y-5">
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Smart Design
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Smart Design
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Collection
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Collection
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Jewelry
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Jewelry
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Cosmetics
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Cosmetics
+              </a>
             </li>
-           </ul>
-          </div>
-          <div class="border-r">
-           <h3 class="mb-2 font-semibold">
+          </ul>
+        </div>
+        <div class="border-r">
+          <h3 class="mb-2 font-semibold">
             Home Group
-           </h3>
-           <ul class="space-y-5">
+          </h3>
+          <ul class="space-y-5">
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Swimsuit
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Swimsuit
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Special
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Special
+              </a>
             </li>
             <li>
-             <a class="text-gray-600 hover:text-black" href="#">
-              Concept
-             </a>
+              <a class="text-gray-600 hover:text-black" href="#">
+                Concept
+              </a>
             </li>
-           </ul>
-          </div>
-          <div>
-              <img src="@/components/icons/menu-element.png"/>
-          </div>
-         </div>
-    </div> 
+          </ul>
+        </div>
+        <div>
+          <img src="@/components/icons/menu-element.png" />
+        </div>
+      </div>
+    </div>
   </div>
- 
+
 </template>
